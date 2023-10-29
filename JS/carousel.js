0d7111c8646b82c1b3616f8d5559d45837a0fd8d
itsolidude <flexibleoli.12@gmail.com>
2023-10-29 19:35:32 -0400

    Fixed bugs in projects.js

--- JS/carousel.js
+++ JS/carousel.js
@@ -25,26 +25,65 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
     ;
 }
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+};
+
+//hide left arrow on first slide, change to sho on settings
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+    else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    }
+    else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+};
+
 //Right button click, move slides right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
+
+    //move dots on arrow click
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide) // return index of next slide
+
     //call function
     moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
     //next slide
     // const amountToMove = nextSlide.style.left; //pixels to move
     // track.style.transform = 'translateX(-' + amountToMove + ')';
     //give current slide class to next slide
     // currentSlide.classList.remove('current-slide');
     //nextSlide.classList.add('current-slide');
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
 
 //left button click, move slides left
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide) // return index of prev slide
+
     //call function
     moveToSlide(track, currentSlide, prevSlide);
+
+    //move dots on arrow click
+    updateDots(currentDot, prevDot);
+
+    //prev slide
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
 
 });
 
@@ -63,9 +102,10 @@ dotsNav.addEventListener('click', e => {
 
     //call function
     moveToSlide(track, currentSlide, targetSlide);
-
-    //update dots
-    currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide');
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
 });
 
+//---------References----------------
+//[1] K. Powell, "How to code a carousel with HTML, CSS and JavaScript - from scratch (part 2)," YouTube, [Video file], Published on [Date Published]. [Online].
+// Available: https://www.youtube.com/watch?v=gBzsE0oieio&t=3424s&ab_channel=KevinPowell
